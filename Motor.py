40c7b97eec287efde63085890575ed8598a14a62
yaffoman <yaffoman@gmail.com>
2020-04-17 23:21:36 -0700

    Added Motor class and methods

---
+++ Motor.py
@@ -0,0 +1,36 @@
+pattern = [(0, 1, 0, 1), (0, 1, 0, 0), (0, 1, 1, 0), (0, 0, 1, 0), (1, 0, 1, 0), (1, 0, 0, 0), (1, 0, 0, 1),
+           (0, 0, 0, 1)]  # halfstep pattern
+delay = .05
+
+
+class Motor:
+    import RPi.GPIO as GPIO
+    import time
+    from time import sleep
+    GPIO.setmode(GPIO.BCM)
+
+    def __init__(self, pin1, pin2, pin3, pin4):
+        self.A1 = pin1
+        self.A2 = pin2
+        self.B1 = pin3
+        self.B2 = pin4
+        GPIO.setup(self.A1, GPIO.OUT)
+        GPIO.setup(self.A2, GPIO.OUT)
+        GPIO.setup(self.B1, GPIO.OUT)
+        GPIO.setup(self.B2, GPIO.OUT)
+        self.state = 0
+        self.move(pattern[self.state])
+
+    def inc(self):
+        self.state = (self.state + 1) % len(pattern)
+        self.move(pattern[self.state])
+
+    def dec(self):
+        self.state = (self.state - 1) % len(pattern)
+        self.move(pattern[self.state])
+
+    def move(self, inpt):
+        GPIO.output(self.A1, inpt[0])
+        GPIO.output(self.A2, inpt[1])
+        GPIO.output(self.B1, inpt[2])
+        GPIO.output(self.B2, inpt[3])
