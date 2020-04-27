aabc4f0ba79958944e838ca38a407a3259535b37
yaffoman <yaffoman@gmail.com>
2020-04-26 21:59:46 -0700

    added wasd control for motors

--- testmotors.py
+++ testmotors.py
@@ -1,6 +1,7 @@
 from Motor import *
 from IOpins import *
 import time
+import keyboard
 
 m1 = Motor(M1A1, M1A2, M1B1, M1B2)
 m2 = Motor(M2A1, M2A2, M2B1, M2B2)
@@ -14,5 +15,17 @@ for i in range(0, 10):
     m2.inc()
     m1.dec()
     time.sleep(.05)
+m1.off()
+m2.off()
 
-
+while True:
+    if keyboard.is_pressed('w'):
+        m1.inc()
+    if keyboard.is_pressed('a'):
+        m1.inc()
+    if keyboard.is_pressed('s'):
+        m1.dec()
+    if keyboard.is_pressed('d'):
+        m1.dec()
+    if keyboard.is_pressed('q'):
+        break
